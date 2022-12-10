0bb6d8c17fa6061c1364cb3d58434cbeaddbe6e1
Kim HyunWoo <stories2@naver.com>
2022-12-11 01:51:32 +0900

    asdfasdf

--- goTokyoMultiProcess.py
+++ goTokyoMultiProcess.py
@@ -95,11 +95,85 @@ def getAreaInfoProcess(areaName, url):
         resizeBrowserHeightAsContentFullHeight(browser)
         saveBase64Image(browser.find_element(By.CSS_SELECTOR, "div.left_block").screenshot_as_base64, f'{areaName}.png')
         slidItemList = getSlideList(browser)
-        logger.info("Get slider item done")
+        logger.info("Get slider item done len {}", len(slidItemList))
     except Exception as e:
         logger.error('Cannot get area info data {}', e)
     finally:
         browser.close()
     logger.info("Area crawling browser closed")
-    exit(0)
+    return 0
 
+def getAreaInfoProcessSingleLine(areaName, url):
+    logger.debug("Area crawling process start, areaName: {}, url: {}", areaName, url)
+    slidItemList = []
+    try:
+        # browser = initHeadlessBrowser()
+        logger.info("Setup headless browser options")
+        chrome_options = Options()
+        options = [
+            "--headless",
+            "--disable-gpu",
+            "--window-size=1920,1080",
+            "--ignore-certificate-errors",
+            "--disable-extensions",
+            "--no-sandbox",
+            "--disable-dev-shm-usage",
+            "start-maximized",
+            "disable-infobars"
+        ]
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'    
+        chrome_options.add_argument('user-agent={0}'.format(user_agent))
+        for option in options:
+            chrome_options.add_argument(option)
+        browser = webdriver.Chrome(options=chrome_options)
+        browser.set_window_size(1920, 1080)
+
+        logger.debug("Browser move to url: {}", url)
+        browser.get(url)
+
+        # resizeBrowserHeightAsContentFullHeight(browser)
+        logger.info("Update browser's height same as content height")
+        requireHeight = browser.execute_script('return document.body.parentNode.scrollHeight')
+        logger.debug("Current content height {}", requireHeight)
+        browser.set_window_size(1920, requireHeight)
+        browser.execute_script("document.querySelector('div.section_banner_top.setheight').style.height = '464px';")
+        logger.info("Manually set banner's height")
+        time.sleep(2) # Because of pictures load time
+
+        saveBase64Image(browser.find_element(By.CSS_SELECTOR, "div.left_block").screenshot_as_base64, f'{areaName}.png')
+
+        # slidItemList = getSlideList(browser)
+
+        sliderCrawlResult = []
+        try:
+            logger.info("Remove banner to load slider list")
+            browser.execute_script("document.querySelector('div.section_banner_top').remove();document.querySelector('div.left_block').remove();jQuery(window).scroll();")
+            time.sleep(10)
+            logger.info("Banner removed")
+        except Exception as e:
+            logger.error("Remove banner has an error {}", e)
+        logger.info("Crawl slider items")
+        sliderCrawlResult = [(
+            slider.find_element(By.CSS_SELECTOR, 'div.slider_ttl > h2').text, #title
+            getSlideItems([item for item in slider.find_elements(By.CSS_SELECTOR, 'div.slick_slide_item')]) #slider's items
+            ) for slider in browser.find_elements(By.CSS_SELECTOR, 'div.section_slider_body')]
+        logger.debug("Crawl slider items result {}", sliderCrawlResult)
+        if sum([len(sliderItemList[1]) for sliderItemList in sliderCrawlResult]) <= 0:
+            logger.info("Slider is empty!");
+
+        logger.info("Get slider item done len {}", len(sliderCrawlResult))
+    except Exception as e:
+        logger.error('Cannot get area info data {}', e)
+    finally:
+        if browser is not None:
+            browser.close()
+            logger.info("Area crawling browser closed")
+    logger.info("Process done!")
+    return 0
+
+def testing(areaName, url):
+    logger.debug("Area crawling process start, areaName: {}, url: {}", areaName, url)
+    time.sleep(10)
+    logger.info("Test process done")
+    return 0
+    # exit(0)
